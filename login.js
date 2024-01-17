dd3747824e8ee200e2dca2d5ad6ef9721cd30105
Pangpangder <90908528+Pangpangder@users.noreply.github.com>
2024-01-17 18:04:27 +0800

    新增登入頁面

---
+++ login.js
@@ -0,0 +1,9 @@
+import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.4.14/vue.esm-browser.min.js';
+let app={
+    data(){
+        return{
+            
+        }
+    }
+};
+createApp(app).mount('#app');
